a0d2da25537a0d1cd4d82bda18f667f84a47effa
PersonTheCat <professionallyperson@gmail.com>
2021-07-25 22:46:55 -0500

    Javadocs for ModDescriptor

--- common/src/main/java/personthecat/catlib/data/ModDescriptor.java
+++ common/src/main/java/personthecat/catlib/data/ModDescriptor.java
@@ -7,6 +7,31 @@ import personthecat.catlib.util.McTools;
 import java.io.File;
 import java.util.Objects;
 
+/**
+ * A platform-agnostic DTO containing information about a given mod's
+ * ID, name, and folder layout. This will be used by the library for
+ * generating commands and command arguments, but it may provide
+ * additional functionalities in the future.
+ * <p>
+ *   Ideally, this object would not be constructed more than once.
+ *   Consider creating this object in a static context and not allowing
+ *   the reference to be modified at any point.
+ * <p>
+ *   This object makes use of {@link Builder} to provide a convenient
+ *   builder API so that any number of arguments can be neatly applied.
+ *   Consider adding a Lombok plugin to your build environment to avoid
+ *   any generated code-related issues in your IDE.
+ * <pre>
+ *   public static final ModDescriptor MOD_DESCRIPTOR =
+ *       ModDescriptor.builder()
+ *           .name("Formatted Name")
+ *           .modId("mod_id")
+ *           .build();
+ * </pre><p>
+ *   Note that <code>name</code> and <code>modId</code> are the only\
+ *   required parameters. The others can be inferred if needed.
+ * </p>
+ */
 @Value
 @Builder
 @SuppressWarnings("unused")
