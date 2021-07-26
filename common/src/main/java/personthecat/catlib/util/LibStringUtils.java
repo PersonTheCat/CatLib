f13794f31fa3ac59f506446b0ac992fcadc013bb
PersonTheCat <professionallyperson@gmail.com>
2021-07-26 09:21:33 -0500

    Javadocs for LibStringUtils

--- common/src/main/java/personthecat/catlib/util/LibStringUtils.java
+++ common/src/main/java/personthecat/catlib/util/LibStringUtils.java
@@ -5,15 +5,29 @@ import lombok.experimental.UtilityClass;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Contains any utilities needed by the library for manipulating miscellaneous
+ * string values. Currently provides a single utility for wrapping text onto
+ * multiple lines, but additional methods may be provided at some point in the
+ * future.
+ */
 @UtilityClass
 public class LibStringUtils {
-    public static List<String> wrapLines(final String description, final int l) {
+
+    /**
+     * Wraps the given text onto multiple lines when given a line length.
+     *
+     * @param text The string value being manipulated.
+     * @param l The maximum number of characters per line.
+     * @return The wrapped lines as a list of strings.
+     */
+    public static List<String> wrapLines(final String text, final int l) {
         final List<String> lines = new ArrayList<>();
         final StringBuilder line = new StringBuilder();
         final StringBuilder word = new StringBuilder();
         boolean nl = false;
 
-        for (final char c : description.toCharArray()) {
+        for (final char c : text.toCharArray()) {
             if (c == '\r') continue;
             if (c == '\n') { nl = true; continue; }
             if (c == ' ' || c == '\t' || nl) {
