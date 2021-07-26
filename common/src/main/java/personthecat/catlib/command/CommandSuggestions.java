0321ec88d0a24fbe2d1b2864227ac27708bca427
PersonTheCat <professionallyperson@gmail.com>
2021-07-25 23:07:06 -0500

    Clean CommandSuggestions

--- common/src/main/java/personthecat/catlib/command/CommandSuggestions.java
+++ common/src/main/java/personthecat/catlib/command/CommandSuggestions.java
@@ -10,26 +10,16 @@ import personthecat.catlib.util.LibReference;
 public class CommandSuggestions {
 
     /** A suggestion provider suggesting that any value is acceptable. */
-    public static final SuggestionProvider<CommandSourceStack> ANY_VALUE = createAnySuggestion();
+    public static final SuggestionProvider<CommandSourceStack> ANY_VALUE =
+        register("any_suggestion", "[<any_value>]");
 
-    public static final SuggestionProvider<CommandSourceStack> ANY_INT = createIntegerSuggestion();
+    /** A suggestion provider suggesting that any integer is acceptable. */
+    public static final SuggestionProvider<CommandSourceStack> ANY_INT =
+        register("integer_suggestion", "[<integer>]", "-1", "0", "1");
 
-    public static final SuggestionProvider<CommandSourceStack> ANY_DECIMAL = createDecimalSuggestion();
-
-    /** Generates the "any value" suggestion provider. */
-    private static SuggestionProvider<CommandSourceStack> createAnySuggestion() {
-        return register("any_suggestion", "[<any_value>]");
-    }
-
-    /** Generates the integer suggestion provider. */
-    private static SuggestionProvider<CommandSourceStack> createIntegerSuggestion() {
-        return register("integer_suggestion", "[<integer>]", "-1", "0", "1");
-    }
-
-    /** Generates the decimal suggestion provider. */
-    private static SuggestionProvider<CommandSourceStack> createDecimalSuggestion() {
-        return register("decimal_suggestion", "[<decimal>]", "0.5", "1.0", "1.5");
-    }
+    /** A suggestion provider suggesting that any decimal is acceptable. */
+    public static final SuggestionProvider<CommandSourceStack> ANY_DECIMAL =
+        register("decimal_suggestion", "[<decimal>]", "0.5", "1.0", "1.5");
 
     private static SuggestionProvider<CommandSourceStack> register(final String name, final String... suggestions) {
         return register(name, (ctx, builder) -> SharedSuggestionProvider.suggest(suggestions, builder));
