22a18be0023569c45de46446d3718220a58d79e8
PersonTheCat <professionallyperson@gmail.com>
2021-07-25 23:11:50 -0500

    Javadocs for CommandSide

--- common/src/main/java/personthecat/catlib/command/CommandSide.java
+++ common/src/main/java/personthecat/catlib/command/CommandSide.java
@@ -1,18 +1,33 @@
 package personthecat.catlib.command;
 
 public enum CommandSide {
+
+    /**
+     * Indicates that a command is valid for and can only run
+     * on the dedicated server side.
+     */
     DEDICATED {
         @Override
         public boolean canRegister(final boolean dedicated) {
             return dedicated;
         }
     },
+
+    /**
+     * Indicates that a command is valid for and can only run
+     * on the integrated, client server side.
+     */
     CLIENT {
         @Override
         public boolean canRegister(final boolean dedicated) {
             return !dedicated;
         }
     },
+
+    /**
+     * Indicates that a command is valid for and can run on
+     * either the dedicated or integrated server side.
+     */
     EITHER {
         @Override
         public boolean canRegister(final boolean dedicated) {
