f9f4b271435a8236d31fb07e15d72bffd061d89c
PersonTheCat <professionallyperson@gmail.com>
2021-07-25 23:09:48 -0500

    Javadocs for CommandType

--- common/src/main/java/personthecat/catlib/command/CommandType.java
+++ common/src/main/java/personthecat/catlib/command/CommandType.java
@@ -1,6 +1,16 @@
 package personthecat.catlib.command;
 
 public enum CommandType {
+
+    /**
+     * Indicates that a command should be applied to the root
+     * command node.
+     */
     GLOBAL,
+
+    /**
+     * Indicates that A command should be applied to the given
+     * mod's main command.
+     */
     MOD
 }
