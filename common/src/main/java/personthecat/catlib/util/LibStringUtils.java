40be3ecb3a310fcf85c1e7e6d368fffcae9651fa
PersonTheCat <professionallyperson@gmail.com>
2021-11-02 21:51:28 -0500

    LibStringUtils#tokenize

--- common/src/main/java/personthecat/catlib/util/LibStringUtils.java
+++ common/src/main/java/personthecat/catlib/util/LibStringUtils.java
@@ -3,6 +3,7 @@ package personthecat.catlib.util;
 import lombok.experimental.UtilityClass;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -101,5 +102,63 @@ public class LibStringUtils {
             }
         }
         return sb.toString();
+    }
+
+    /**
+     * Separates a continuous string of alphanumeric characters into an array
+     * of words.
+     *
+     * <p>
+     *   For example, the following input:
+     * </p><code>
+     *   oneTwoThree
+     * </code>
+     * <p>
+     *   Will be converted to this array:
+     * </p><code>
+     *   [ "one", "Two", "Three" ]
+     * </code>
+     *
+     * @param camel The input text in either camel or pascal case.
+     * @return An array of words extracted from the text.
+     */
+    public static List<String> tokenize(final String camel) {
+        if (camel.isEmpty()) return Collections.emptyList();
+        final List<String> tokens = new ArrayList<>();
+        int index = 0;
+        while (index < camel.length()) {
+            tokens.add(camel.substring(index, index = getNextWord(camel, index)));
+        }
+        return tokens;
+    }
+
+    /**
+     * Returns the next index of a <em>word</em> in the given text.
+     *
+     * <p>Note that the given text is assumed to contain no whitespace.
+     * For this reason, the algorithm may not be ideal for the majority
+     * of use cases and thus is not exposed here.
+     *
+     * @param camel The input text in camel or pascal case.
+     * @param index The starting index to begin searching from.
+     * @return The next word, or else the end of the text.
+     */
+    private static int getNextWord(final String camel, int index) {
+        char c = camel.charAt(index);
+        int numCapital = 0;
+        while (Character.isUpperCase(c) && index < camel.length() - 1) {
+            c = camel.charAt(++index);
+            numCapital++;
+        }
+        // The last character in abbreviations is usually a new word
+        if (numCapital > 1) {
+            index--;
+        } else {
+            while (Character.isLowerCase(c) && index < camel.length() - 1) {
+                c = camel.charAt(++index);
+            }
+        }
+        // Get exclusive range for last word.
+        return index == camel.length() - 1 ? camel.length() : index;
     }
 }
