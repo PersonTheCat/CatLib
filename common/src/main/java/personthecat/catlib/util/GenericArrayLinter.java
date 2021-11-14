493dc2a2065b2fa3980637d040335680d0b95a75
PersonTheCat <professionallyperson@gmail.com>
2021-11-13 22:08:31 -0600

    Better array pattern

--- common/src/main/java/personthecat/catlib/util/GenericArrayLinter.java
+++ common/src/main/java/personthecat/catlib/util/GenericArrayLinter.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class GenericArrayLinter extends SyntaxLinter {
 
-    private static final Pattern ELEMENT_PATTERN = Pattern.compile("[^\\[\\],\\s]");
+    private static final Pattern ELEMENT_PATTERN = Pattern.compile("[^\\[\\],]+");
 
     private static final Target[] TARGETS = {
         new Target(ELEMENT_PATTERN, RANDOM_COLOR)
