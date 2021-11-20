2e944b3bfbcf29ae39a29429576ddfe166221c2e
PersonTheCat <professionallyperson@gmail.com>
2021-11-19 20:57:56 -0600

    Ongoing client ready event testing

--- common/src/main/java/personthecat/catlib/mixin/TitleScreenMixin.java
+++ common/src/main/java/personthecat/catlib/mixin/TitleScreenMixin.java
@@ -1,6 +1,6 @@
 package personthecat.catlib.mixin;
 
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,12 +9,12 @@ import personthecat.catlib.event.lifecycle.GameReadyEvent;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
-@Mixin(Minecraft.class)
-public abstract class MinecraftMixin {
+@Mixin(TitleScreen.class)
+public abstract class TitleScreenMixin {
 
     private static final AtomicBoolean INIT_COMPLETE = new AtomicBoolean(false);
 
-    @Inject(method = "setScreen", at = @At("HEAD"))
+    @Inject(method = "init", at = @At("TAIL"))
     public void postInit(final CallbackInfo ci) {
         if (INIT_COMPLETE.compareAndSet(false, true)) {
             GameReadyEvent.CLIENT.invoker().run();
