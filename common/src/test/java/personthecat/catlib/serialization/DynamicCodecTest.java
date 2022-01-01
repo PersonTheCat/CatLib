52bac3a95ced22aee6cbe8ee742a2636766b6d06
PersonTheCat <professionallyperson@gmail.com>
2022-01-01 12:22:44 -0600

    Test cases for DynamicCodec

---
+++ common/src/test/java/personthecat/catlib/serialization/DynamicCodecTest.java
@@ -0,0 +1,127 @@
+package personthecat.catlib.serialization;
+
+import com.mojang.datafixers.util.Pair;
+import com.mojang.serialization.Codec;
+import org.hjson.JsonObject;
+import org.hjson.JsonValue;
+import org.jetbrains.annotations.Nullable;
+import org.junit.jupiter.api.Test;
+
+import static personthecat.catlib.serialization.CodecUtils.dynamic;
+import static personthecat.catlib.serialization.DynamicField.extend;
+import static personthecat.catlib.serialization.DynamicField.field;
+import static personthecat.catlib.serialization.DynamicField.recursive;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class DynamicCodecTest {
+
+    @Test
+    public void simpleDynamicCodec_mapsValues() {
+        final JsonObject json = parse("a:'test',b:1337");
+        final SimpleObject o = decode(SimpleObject.CODEC, json);
+        assertNotNull(o);
+        assertEquals("test", o.a);
+        assertEquals(1337, o.b);
+    }
+
+    @Test
+    public void simpleDynamicCodec_supportsMissingValues() {
+        final JsonObject json = parse("");
+        final SimpleObject o = decode(SimpleObject.CODEC, json);
+        assertNotNull(o);
+        assertNull(o.a);
+        assertEquals(0, o.b);
+    }
+
+    @Test
+    public void recursiveCodec_mapsRecursively() {
+        final JsonObject json = parse("a:'t1',b:{a:'t2',b:{a:'t3'}}");
+        final RecursiveObject o = decode(RecursiveObject.CODEC, json);
+        assertNotNull(o);
+        assertEquals("t1", o.a);
+        assertNotNull(o.b);
+        assertEquals("t2", o.b.a);
+        assertNotNull(o.b.b);
+        assertEquals("t3", o.b.b.a);
+        assertNull(o.b.b.b);
+    }
+
+    @Test
+    public void recursiveCodec_supportsTerminalValues() {
+        final JsonObject json = parse("");
+        final RecursiveObject o = decode(RecursiveObject.CODEC, json);
+        assertNotNull(o);
+        assertNull(o.a);
+        assertNull(o.b);
+    }
+
+    @Test
+    public void extendingCodec_simulatesInheritance() {
+        final JsonObject json = parse("a:'test',b:1337");
+        final ExtendingObject o = decode(ExtendingObject.CODEC, json);
+        assertNotNull(o);
+        assertEquals("test", o.a);
+        assertNotNull(o.i);
+        assertEquals(1337, o.i.b);
+    }
+
+    @Test
+    public void extendingCodec_toleratesMissingField() {
+        final JsonObject json = parse("");
+        final ExtendingObject o = decode(ExtendingObject.CODEC, json);
+        assertNotNull(o);
+        assertNull(o.a);
+        assertNotNull(o.i);
+        assertEquals(0, o.i.b);
+    }
+
+    private static JsonObject parse(final String json) {
+        return JsonObject.readHjson(json).asObject();
+    }
+
+    @Nullable
+    private static <T> T decode(final Codec<T> codec, final JsonValue value) {
+        final Pair<T, JsonValue> pair = codec.decode(HjsonOps.INSTANCE, value).get().left().orElse(null);
+        return pair != null ? pair.getFirst() : null;
+    }
+
+    static class SimpleObject {
+        @Nullable String a;
+        int b;
+
+        static Codec<SimpleObject> CODEC = dynamic(SimpleObject::new).create(
+            field(Codec.STRING, "a", o -> o.a, (o, a) -> o.a = a),
+            field(Codec.INT, "b", o -> o.b, (o, b) -> o.b = b)
+        );
+    }
+
+    static class RecursiveObject {
+        @Nullable String a;
+        @Nullable RecursiveObject b;
+
+        static Codec<RecursiveObject> CODEC = dynamic(RecursiveObject::new).create(
+            field(Codec.STRING, "a", o -> o.a, (o, a) -> o.a = a),
+            recursive("b", o -> o.b, (o, b) -> o.b = b)
+        );
+    }
+
+    static class ExtendingObject {
+        @Nullable String a;
+        InnerObject i = new InnerObject();
+
+        static Codec<ExtendingObject> CODEC = dynamic(ExtendingObject::new).create(
+            field(Codec.STRING, "a", o -> o.a, (o, a) -> o.a = a),
+            extend(InnerObject.CODEC, "i", o -> o.i, (o, i) -> o.i = i)
+        );
+    }
+
+    static class InnerObject {
+        int b;
+
+        static Codec<InnerObject> CODEC = dynamic(InnerObject::new).create(
+            field(Codec.INT, "b", o -> o.b, (o, b) -> o.b = b)
+        );
+    }
+}
