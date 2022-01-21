eb13c79596590e45bc0a903f7147cd47106f0351
PersonTheCat <professionallyperson@gmail.com>
2022-01-21 08:59:09 -0600

    Handle a few edge cases for dim injection

--- common/src/main/java/personthecat/catlib/mixin/ChunkGeneratorMixin.java
+++ common/src/main/java/personthecat/catlib/mixin/ChunkGeneratorMixin.java
@@ -5,10 +5,13 @@ import net.minecraft.world.level.dimension.DimensionType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import personthecat.catlib.util.DimInjector;
 
 @Mixin(ChunkGenerator.class)
 public class ChunkGeneratorMixin implements DimInjector {
+
+    @Unique
     DimensionType catlibInjectedDim;
 
     @Override
