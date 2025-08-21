db7b4816d7912c5ec32b9011ca185931ac8d66e6
Dalton Richardson <professionallyperson@gmail.com>
2025-08-21 13:45:57 -0500

    Add surface rules to CommonRegistries

--- common/src/main/java/personthecat/catlib/registry/CommonRegistries.java
+++ common/src/main/java/personthecat/catlib/registry/CommonRegistries.java
@@ -12,6 +12,8 @@ import net.minecraft.world.level.biome.BiomeSource;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.DensityFunction;
+import net.minecraft.world.level.levelgen.SurfaceRules.ConditionSource;
+import net.minecraft.world.level.levelgen.SurfaceRules.RuleSource;
 import net.minecraft.world.level.levelgen.carver.WorldCarver;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProviderType;
@@ -31,6 +33,8 @@ public class CommonRegistries {
     public static final RegistryHandle<Item> ITEM = RegistryUtils.getHandle(Registries.ITEM);
     public static final RegistryHandle<EntityType<?>> ENTITY = RegistryUtils.getHandle(Registries.ENTITY_TYPE);
     public static final RegistryHandle<MapCodec<? extends BiomeSource>> BIOME_SOURCE = RegistryUtils.getHandle(Registries.BIOME_SOURCE);
+    public static final RegistryHandle<MapCodec<? extends ConditionSource>> MATERIAL_CONDITION = RegistryUtils.getHandle(Registries.MATERIAL_CONDITION);
+    public static final RegistryHandle<MapCodec<? extends RuleSource>> MATERIAL_RULE = RegistryUtils.getHandle(Registries.MATERIAL_RULE);
     public static final RegistryHandle<MapCodec<? extends ChunkGenerator>> CHUNK_GENERATOR = RegistryUtils.getHandle(Registries.CHUNK_GENERATOR);
     public static final RegistryHandle<MapCodec<? extends DensityFunction>> DENSITY_FUNCTION_TYPE = RegistryUtils.getHandle(Registries.DENSITY_FUNCTION_TYPE);
     public static final RegistryHandle<WorldCarver<?>> CARVER = RegistryUtils.getHandle(Registries.CARVER);
