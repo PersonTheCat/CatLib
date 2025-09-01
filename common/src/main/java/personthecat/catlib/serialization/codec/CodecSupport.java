0435baa297bd849b77184f32ad24204b0315c035
Dalton Richardson <professionallyperson@gmail.com>
2025-09-01 14:26:44 -0500

    CodecSupport#tryGetCodec by registry key

--- common/src/main/java/personthecat/catlib/serialization/codec/CodecSupport.java
+++ common/src/main/java/personthecat/catlib/serialization/codec/CodecSupport.java
@@ -19,12 +19,17 @@ import java.util.function.Function;
 public final class CodecSupport {
 
     private static final Map<Class<?>, Function<Object, Codec<?>>> CODECS_BY_TYPE = new ConcurrentHashMap<>();
+    private static final Map<ResourceKey<?>, Codec<?>> CODECS_BY_KEY = new ConcurrentHashMap<>();
 
     private CodecSupport() {}
 
     @SuppressWarnings({"unchecked", "rawtypes"})
-    public static <T> void registerGetter(final Class<T> clazz, final Function<? extends T, Codec<?>> getter) {
+    public static <T> void registerGetter(final Class<T> clazz, final Function<? extends T, Codec<? extends T>> getter) {
         CODECS_BY_TYPE.put(clazz, (Function) getter);
+    }
+
+    public static <T> void registerCodec(final ResourceKey<T> key, final Codec<? extends T> codec) {
+        CODECS_BY_KEY.put(key, codec);
     }
 
     /**
@@ -64,22 +69,33 @@ public final class CodecSupport {
      * @param <T> The type of object being inspected.
      * @return The {@link Codec codec}, else <code>null</code>.
      */
+    @SuppressWarnings("ConstantConditions") // definitely does return null
     public static <T> @Nullable Codec<T> tryGetCodec(final T t) {
         if (t == null) return null;
         final Function<Object, Codec<?>> c = CODECS_BY_TYPE.computeIfAbsent(t.getClass(), clazz -> {
-            final Codec<?> codec = getFromDataLoader(t);
+            final ResourceKey<? extends Registry<?>> key =
+                RegistryUtils.tryGetByType(t.getClass()).map(RegistryHandle::key).orElse(null);
+            final Codec<?> codec = tryGetCodec(key);
             return codec != null ? o -> codec : null;
         });
         return c != null ? cast(c.apply(t)) : null;
     }
 
-    @SuppressWarnings("DataFlowIssue") // definitely does return null
-    private static @Nullable Codec<?> getFromDataLoader(final Object t) {
-        final ResourceKey<? extends Registry<?>> key =
-            RegistryUtils.tryGetByType(t.getClass()).map(RegistryHandle::key).orElse(null);
-        if (key == null) {
-            return null;
-        }
+    /**
+     * Resolves a {@link Codec codec} for the given {@link ResourceKey key}, either
+     * from the cache or directly from {@link RegistryDataLoader}.
+     *
+     * @param key The key of the registry corresponding to the codec.
+     * @param <T> The type of registry and codec.
+     * @return The codec itself, or else <code>null</code>.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> @Nullable Codec<T> tryGetCodec(final ResourceKey<? extends Registry<T>> key) {
+        if (key == null) return null;
+        return cast(CODECS_BY_KEY.computeIfAbsent(key, k -> getFromDataLoader((ResourceKey<? extends Registry<?>>) k)));
+    }
+
+    private static @Nullable Codec<?> getFromDataLoader(ResourceKey<? extends Registry<?>> key) {
         Codec<?> c = lookup(RegistryDataLoader.WORLDGEN_REGISTRIES, key);
         if (c != null) return c;
         c = lookup(RegistryDataLoader.DIMENSION_REGISTRIES, key);
